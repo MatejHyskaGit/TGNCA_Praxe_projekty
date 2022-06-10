e102c3510be17178d32b959fb5041a7ba79e1207
Matěj Hyška <matej.hyska.020@pslib.cz>
2022-06-10 12:42:58 +0200

    deleted useless comments

--- examples/googleAnalytics.php
+++ examples/googleAnalytics.php
@@ -15,8 +15,8 @@ $day = "2022-04-15";
 
 $output_array = [];
 
-$dim_array = ["ga:productName","ga:campaign"];
-$met_array = ["ga:itemRevenue","ga:itemQuantity","ga:uniquePurchases","ga:itemsPerPurchase"];
+$dim_array = ["ga:productName", "ga:campaign"];
+$met_array = ["ga:itemRevenue", "ga:itemQuantity", "ga:uniquePurchases", "ga:itemsPerPurchase"];
 
 $response = getReport($analytics, $view_id, $day, $dim_array, $met_array);
 $output_array = convertData($output_array, $response);
@@ -66,48 +66,6 @@ function convertData($output_array, $response)
                 print("<br>");
                 print("<br>");
                 print("<br>");
-                // $camp_id = get_campaign_id_from_utm_campaign($camp_name, $eshop["id"]);
-                // //if not exist
-                // $date = date("Y-m-d", strtotime($date_time));
-
-                // $campaign_day = do_query("SELECT DISTINCT * FROM campaign_days WHERE id_cam='" . $camp_id . "' AND id_day='" . $date . "'");
-                // $row = $campaign_day->fetch();
-
-                // if ($eshop["currency_analytics"] == "EUR" && $eshop["currency_ads"] == "CZ") {
-                //     //echo "převádím";
-                //     $curr_rate = load_curr_rate_on_day($date);
-                //     $revenue = $revenue * $curr_rate;
-                // }
-                // if ($eshop["currency_analytics"] == "CZ" && $eshop["currency_ads"] == "EUR") {
-                //     //echo "převádím";
-                //     $curr_rate = load_curr_rate_on_day($date);
-                //     $revenue = $revenue / $curr_rate;
-                // }
-                // $found = false;
-                // for ($x = 0; $x <= sizeof($campaigns_info); $x++) {
-                //     //foreach ($campaigns_info as $campaigns_info_val) {
-                //     if ($campaigns_info[$x]["camp_id"] == $camp_id) {
-
-                //         $convs = $ecom_conv_rate * $sessions / 100;
-                //         $convs_old = $campaigns_info[$x]["conv_rate"] * $campaigns_info[$x]["sessions"] / 100;
-                //         $campaigns_info[$x]["conv_rate"] = ($convs + $convs_old) / ($sessions + $campaigns_info[$x]["sessions"]) * 100;
-
-                //         $campaigns_info[$x]["users"] += $users;
-                //         $campaigns_info[$x]["sessions"] += $sessions;
-                //         $campaigns_info[$x]["revenue"] += $revenue;
-                //         $campaigns_info[$x]["transactions"] += $transactions;
-                //         $campaigns_info[$x]["clicks"] += $clicks;
-                //         $campaigns_info[$x]["costPerConversion"] += $cost_per_conv;
-                //         $campaigns_info[$x]["CPC"] += $CPC;
-                //         $campaigns_info[$x]["RPC"] += $RPC;
-
-                //         $found = true;
-                //         break;
-                //     }
-                // }
-                // if (!$found) {
-                //     array_push($campaigns_info, ["date_time" => $date_time, "camp_id" => $camp_id, "users" => $users, "sessions" => $sessions, "revenue" => $revenue, "conv_rate" => $ecom_conv_rate, "transactions" => $transactions, "clicks" => $clicks, "ga:goalConversionRateAll" => $cost_per_conv, "CPC" => $CPC, "RPC" => $RPC]);
-                // }
             }
         }
     }
@@ -157,62 +115,3 @@ function getReport($analytics, $VIEW_ID, $day = "today", $dim_array, $met_array)
     $body->setReportRequests(array($request));
     return $analytics->reports->batchGet($body);
 }
-
-
-
-
-
-
-
-// function getReport_Delete($analytics, $VIEW_ID, $day = "today")
-// {
-//     // Replace with your view ID, for example XXXX.
-//     // Create the DateRange object.
-//     $dateRange = new Google_Service_AnalyticsReporting_DateRange();
-//     $dateRange->setStartDate($day);
-//     $dateRange->setEndDate($day);
-
-//     // Creating Dimensions
-//     function newGADimension($ga_name)
-//     {
-//         $ga_temp = new Google_Service_AnalyticsReporting_Dimension();
-//         $ga_temp->setName($ga_name);
-//         return $ga_temp;
-//     }
-
-
-//     $keyword = newGADimension("ga:keyword");
-
-//     $campaign = newGADimension("ga:campaign");
-
-//     $source = newGADimension("ga:source");
-
-//     $medium = newGADimension("ga:medium");
-
-//     $date_time = newGADimension("ga:date");
-
-//     // Create the Metrics objects.
-//     $sessions = new Google_Service_AnalyticsReporting_Metric();
-//     $sessions->setExpression("ga:sessions");
-//     $sessions->setAlias("sessions");
-
-//     $users = newMetric("ga:users", "users");
-//     $revenue = newMetric("ga:transactionRevenue", "revenue");
-//     $transactions = newMetric("ga:transactions", "transactions");
-//     $conv_rate = newMetric("ga:transactionsPerSession", "conv_rate");
-//     $clicks = newMetric("ga:adClicks", "clicks");
-//     $cost_per_conv = newMetric("ga:costPerConversion", "cost_per_conv");
-//     $CPC = newMetric("ga:CPC", "CPC");
-//     $RPC = newMetric("ga:RPC", "RPC");
-
-//     // Create the ReportRequest object.
-//     $request = new Google_Service_AnalyticsReporting_ReportRequest();
-//     $request->setViewId($VIEW_ID);
-//     $request->setDateRanges($dateRange);
-//     $request->setDimensions(array($source, $medium, $campaign/* , $keyword */, $date_time));
-//     $request->setMetrics(array($sessions, $users, $revenue, $transactions, $conv_rate, $clicks, $cost_per_conv, $CPC, $RPC));
-
-//     $body = new Google_Service_AnalyticsReporting_GetReportsRequest();
-//     $body->setReportRequests(array($request));
-//     return $analytics->reports->batchGet($body);
-// }
--- googleAnalytics.php
+++ googleAnalytics.php
@@ -8,7 +8,6 @@ include_once $_SERVER['DOCUMENT_ROOT'] . "/" . "data/main_vars.php";
 include_once PATH_TO_ANALYTICS_LOADER;
 $analytics = load_analytics_by_id(1);
 
-// $eshops = get_all_clients_from_DB();
 $eshop = load_eshop(233);
 $view_id = "" . $eshop["view_id"];
 $day = "2022-04-15";
@@ -59,55 +58,11 @@ function convertData($output_array, $response)
                         }
                     }
                 }
-                // $camp_id = get_campaign_id_from_utm_campaign($camp_name, $eshop["id"]);
-                // //if not exist
-                // $date = date("Y-m-d", strtotime($date_time));
-
-                // $campaign_day = do_query("SELECT DISTINCT * FROM campaign_days WHERE id_cam='" . $camp_id . "' AND id_day='" . $date . "'");
-                // $row = $campaign_day->fetch();
-
-                // if ($eshop["currency_analytics"] == "EUR" && $eshop["currency_ads"] == "CZ") {
-                //     //echo "převádím";
-                //     $curr_rate = load_curr_rate_on_day($date);
-                //     $revenue = $revenue * $curr_rate;
-                // }
-                // if ($eshop["currency_analytics"] == "CZ" && $eshop["currency_ads"] == "EUR") {
-                //     //echo "převádím";
-                //     $curr_rate = load_curr_rate_on_day($date);
-                //     $revenue = $revenue / $curr_rate;
-                // }
-                // $found = false;
-                // for ($x = 0; $x <= sizeof($campaigns_info); $x++) {
-                //     //foreach ($campaigns_info as $campaigns_info_val) {
-                //     if ($campaigns_info[$x]["camp_id"] == $camp_id) {
-
-                //         $convs = $ecom_conv_rate * $sessions / 100;
-                //         $convs_old = $campaigns_info[$x]["conv_rate"] * $campaigns_info[$x]["sessions"] / 100;
-                //         $campaigns_info[$x]["conv_rate"] = ($convs + $convs_old) / ($sessions + $campaigns_info[$x]["sessions"]) * 100;
-
-                //         $campaigns_info[$x]["users"] += $users;
-                //         $campaigns_info[$x]["sessions"] += $sessions;
-                //         $campaigns_info[$x]["revenue"] += $revenue;
-                //         $campaigns_info[$x]["transactions"] += $transactions;
-                //         $campaigns_info[$x]["clicks"] += $clicks;
-                //         $campaigns_info[$x]["costPerConversion"] += $cost_per_conv;
-                //         $campaigns_info[$x]["CPC"] += $CPC;
-                //         $campaigns_info[$x]["RPC"] += $RPC;
-
-                //         $found = true;
-                //         break;
-                //     }
-                // }
-                // if (!$found) {
-                //     array_push($campaigns_info, ["date_time" => $date_time, "camp_id" => $camp_id, "users" => $users, "sessions" => $sessions, "revenue" => $revenue, "conv_rate" => $ecom_conv_rate, "transactions" => $transactions, "clicks" => $clicks, "ga:goalConversionRateAll" => $cost_per_conv, "CPC" => $CPC, "RPC" => $RPC]);
-                // }
             }
         }
     }
     return $output_array;
 }
-
-
 
 function getReport($analytics, $VIEW_ID, $day = "today", $dim_array, $met_array)
 {
@@ -150,62 +105,3 @@ function getReport($analytics, $VIEW_ID, $day = "today", $dim_array, $met_array)
     $body->setReportRequests(array($request));
     return $analytics->reports->batchGet($body);
 }
-
-
-
-
-
-
-
-// function getReport_Delete($analytics, $VIEW_ID, $day = "today")
-// {
-//     // Replace with your view ID, for example XXXX.
-//     // Create the DateRange object.
-//     $dateRange = new Google_Service_AnalyticsReporting_DateRange();
-//     $dateRange->setStartDate($day);
-//     $dateRange->setEndDate($day);
-
-//     // Creating Dimensions
-//     function newGADimension($ga_name)
-//     {
-//         $ga_temp = new Google_Service_AnalyticsReporting_Dimension();
-//         $ga_temp->setName($ga_name);
-//         return $ga_temp;
-//     }
-
-
-//     $keyword = newGADimension("ga:keyword");
-
-//     $campaign = newGADimension("ga:campaign");
-
-//     $source = newGADimension("ga:source");
-
-//     $medium = newGADimension("ga:medium");
-
-//     $date_time = newGADimension("ga:date");
-
-//     // Create the Metrics objects.
-//     $sessions = new Google_Service_AnalyticsReporting_Metric();
-//     $sessions->setExpression("ga:sessions");
-//     $sessions->setAlias("sessions");
-
-//     $users = newMetric("ga:users", "users");
-//     $revenue = newMetric("ga:transactionRevenue", "revenue");
-//     $transactions = newMetric("ga:transactions", "transactions");
-//     $conv_rate = newMetric("ga:transactionsPerSession", "conv_rate");
-//     $clicks = newMetric("ga:adClicks", "clicks");
-//     $cost_per_conv = newMetric("ga:costPerConversion", "cost_per_conv");
-//     $CPC = newMetric("ga:CPC", "CPC");
-//     $RPC = newMetric("ga:RPC", "RPC");
-
-//     // Create the ReportRequest object.
-//     $request = new Google_Service_AnalyticsReporting_ReportRequest();
-//     $request->setViewId($VIEW_ID);
-//     $request->setDateRanges($dateRange);
-//     $request->setDimensions(array($source, $medium, $campaign/* , $keyword */, $date_time));
-//     $request->setMetrics(array($sessions, $users, $revenue, $transactions, $conv_rate, $clicks, $cost_per_conv, $CPC, $RPC));
-
-//     $body = new Google_Service_AnalyticsReporting_GetReportsRequest();
-//     $body->setReportRequests(array($request));
-//     return $analytics->reports->batchGet($body);
-// }
